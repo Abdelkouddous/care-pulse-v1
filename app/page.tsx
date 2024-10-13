b4248650fea242d3d6c4dac718e2f345d1c5ebc4
Abdelkouddous <58855794+Abdelkouddous@users.noreply.github.com>
2024-10-13 21:11:04 +0100

    restored home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-const page = () => {
-  return <div>page</div>;
-};
+import PatientForm from "@/components/forms/PatientForm";
+// import { SiteFooter } from "@/components/site-footer";
+import { PasskeyModal } from "@/components/PasskeyModal";
 
-export default page;
+export default async function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams?.admin === "true";
+
+  // const isPatient = searchParams?.patient === "true";
+
+  // const MyComponent = () => {
+  //   useScrollAnimation(".animate-on-scroll");
+
+  //   return (
+  //     <div>
+  //       <div className="animate-on-scroll fade-in">Content that fades in on scroll</div>
+  //       <div className="animate-on-scroll fade-in">Another content block</div>
+  //     </div>
+  //   );
+  // };
+
+  // export default MyComponent;
+  return (
+    <div>
+      <div className="flex justify-stretch m-auto p-3 h-screen fade-in-page">
+        {/* OTP Verification  */}
+
+        {isAdmin && <PasskeyModal></PasskeyModal>}
+
+        <Image
+          src="/assets/main.jpg"
+          height={750}
+          width={750}
+          alt="Doctor"
+          className=" side-img max-w-[50%] m-3 mb-2  opacity-90 border-r-8 border-l-dark-200 border-spacing-10 "
+        ></Image>
+
+        <section className="remove-scrollbar container my-auto ">
+          <PatientForm></PatientForm>
+        </section>
+      </div>
+
+      <div className="flex justify-center items-center ">
+        {/* <SiteFooter></SiteFooter> */}
+      </div>
+    </div>
+  );
+}
